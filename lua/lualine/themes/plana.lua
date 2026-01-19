6aef23ca8b45e8fba389963462316849bce40b98
tsssni <dingyongyu2002@foxmail.com>
2026-01-19 23:44:18 +0800

    optimize lualine color for navic

--- lua/lualine/themes/plana.lua
+++ lua/lualine/themes/plana.lua
@@ -1,11 +1,11 @@
 local palette = require("plana.palette")
 local theme = {
-	a = { fg = palette.cyan },
-	b = { fg = palette.light_cyan },
-	c = { fg = palette.light_blue },
-	x = { fg = palette.light_green },
-	y = { fg = palette.blue },
-	z = { fg = palette.light_magenta },
+  a = { fg = palette.cyan },
+  b = { fg = palette.light_cyan },
+  c = { fg = palette.light_green },
+  x = { fg = palette.light_magenta },
+  y = { fg = palette.blue },
+  z = { fg = palette.light_blue },
 }
 
 return {
--- lua/plana/palette.lua
+++ lua/plana/palette.lua
@@ -1,30 +1,30 @@
 local palette = {
-	front = '#bebad9',
-	back = '#303446',
+  front = '#bebad9',
+  back = '#303446',
 
-	black = '#1a1730',
-	light_black = '#645e8c',
+  black = '#1a1730',
+  light_black = '#645e8c',
 
-	red = '#f26dab',
-	light_red = '#ff8d7c',
+  red = '#f26dab',
+  light_red = '#ff8d7c',
 
-	green = '#8ec794',
-	light_green = '#a8e5af',
+  green = '#8ec794',
+  light_green = '#a8e5af',
 
-	yellow = '#e5c890',
-	light_yellow = '#f5dba8',
+  yellow = '#e5c890',
+  light_yellow = '#f5dba8',
 
-	blue = '#8499d9',
-	light_blue = '#a0b4e5',
+  blue = '#8499d9',
+  light_blue = '#a0b4e5',
 
-	magenta = '#a78dd4',
-	light_magenta = '#c0a8e8',
+  magenta = '#a78dd4',
+  light_magenta = '#c0a8e8',
 
-	cyan = '#7dd5d5',
-	light_cyan = '#9fe5e5',
+  cyan = '#7dd5d5',
+  light_cyan = '#9fe5e5',
 
-	white = '#bebad9',
-	light_white = '#d5d1e6',
+  white = '#bebad9',
+  light_white = '#d5d1e6',
 }
 
 return palette
